978fd1071593ebc9283868832a5f46ead5b1e8e6
Andres Rodriguez <androdriguez14@gmail.com>
2022-06-15 13:11:49 -0400

    [ADD] notification-channel and variables

---
+++ notification-channel.tf
@@ -0,0 +1,35 @@
+# Setting up provider
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.24.0"
+    }
+  }
+}
+provider "google" {
+  credentials = file("${var.json_key_path}")
+  project     = var.project
+}
+
+# Enabling APIs for the service to run
+resource "google_project_service" "project_services" {
+  for_each                   = toset(var.activate_apis)
+  project                    = var.project
+  service                    = each.value
+  disable_on_destroy         = false
+  disable_dependent_services = false
+}
+
+# Notifications to Slack
+resource "google_monitoring_notification_channel" "slack_notifications" {
+  type         = "slack"
+  display_name = "#tfe_support_channel"
+  project      = var.project
+  labels = {
+    channel_name = "#umami-support-channel"
+  }
+  sensitive_labels {
+    auth_token = var.slack-auth-token
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,94 @@
+
+variable "project" {
+  default = "analytics-353001"
+}
+
+variable "json_key_path" {
+}
+
+variable "slack-auth-token" {
+}
+
+variable "activate_apis" {
+  description = "The list of apis to activate within the project"
+  type        = list(string)
+  default = [
+    "monitoring.googleapis.com",
+    "logging.googleapis.com",
+    "osconfig.googleapis.com"
+  ]
+}
+
+# vm policies
+variable "vm_filter" {
+    default = ""
+}
+
+variable "vm_cpu_alert_policy_parameters" {
+  default = {
+    auto_close_interval         = "604800s",
+    threshold_duration_trouble  = "300s",
+    threshold_value_trouble     = 0.8,
+    threshold_duration_critical = "300s",
+    threshold_value_critical    = 0.9,
+    documentation               = "Umami monitoring",
+  }
+}
+variable "vm_mem_alert_policy_parameters" {
+  default = {
+    auto_close_interval         = "604800s",
+    threshold_duration_trouble  = "300s",
+    threshold_value_trouble     = 80,
+    threshold_duration_critical = "300s",
+    threshold_value_critical    = 90,
+    documentation               = "Umami monitoring",
+  }
+}
+variable "vm_disk_alert_policy_parameters" {
+  default = {
+    auto_close_interval         = "604800s",
+    threshold_duration_trouble  = "300s",
+    threshold_value_trouble     = 80,
+    threshold_duration_critical = "300s",
+    threshold_value_critical    = 90,
+    filter                      = "metric.label.\"device\"!=monitoring.regex.full_match(\"/dev/loop.*\") metric.label.\"device\"!=monitoring.regex.full_match(\"loop.*\")"
+    documentation               = "Umami monitoring",
+  }
+}
+
+# cloudsql policies
+variable "cloudsql_filter" {
+    default = ""
+}
+
+variable "cloudsql_cpu_alert_policy_parameters" {
+  default = {
+    auto_close_interval         = "604800s",
+    threshold_duration_trouble  = "300s",
+    threshold_value_trouble     = 0.8,
+    threshold_duration_critical = "300s",
+    threshold_value_critical    = 0.9,
+    documentation               = "Umami monitoring",
+  }
+}
+variable "cloudsql_mem_alert_policy_parameters" {
+  default = {
+    auto_close_interval         = "604800s",
+    threshold_duration_trouble  = "300s",
+    threshold_value_trouble     = 0.8,
+    threshold_duration_critical = "300s",
+    threshold_value_critical    = 0.9,
+    documentation               = "Umami monitoring",
+  }
+}
+variable "cloudsql_disk_alert_policy_parameters" {
+  default = {
+    auto_close_interval         = "604800s",
+    threshold_duration_trouble  = "300s",
+    threshold_value_trouble     = 0.8,
+    threshold_duration_critical = "300s",
+    threshold_value_critical    = 0.9,
+    filter                      = "metric.label.\"device\"!=monitoring.regex.full_match(\"/dev/loop.*\") metric.label.\"device\"!=monitoring.regex.full_match(\"loop.*\")"
+    documentation               = "Umami monitoring",
+  }
+}
